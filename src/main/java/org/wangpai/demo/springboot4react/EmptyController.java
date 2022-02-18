e8d2a3e376fdf0a1c8ed9325823d1bc69a5e1462
wangpai <contactwangpai@163.com>
2022-02-19 04:14:12 +0800

    2022年2月19日-使用 Spring MVC 控制器导向 React

--- src/main/java/org/wangpai/demo/springboot4react/EmptyController.java
+++
@@ -1,7 +0,0 @@
-package org.wangpai.demo.springboot4react;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class EmptyController {
-}
---
+++ src/main/java/org/wangpai/demo/springboot4react/ReactController.java
@@ -0,0 +1,13 @@
+package org.wangpai.demo.springboot4react;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReactController {
+    @GetMapping("/index")
+    public String index(Model model) {
+        return "index";
+    }
+}
